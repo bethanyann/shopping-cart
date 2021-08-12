ee665ab4c95f6fd1195869057e53311bfbed8358
bethanyann <bethanyann44@gmail.com>
2021-08-12 12:09:19 -0500

    app styles

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,4 +1,14 @@
 import styled from'styled-components';
+import IconButton from '@material-ui/core/IconButton';
 
+//styled componenets
+export const StyledWrapper = styled.div`
+    margin: 40px;
+`;
 
-export const StyledWrapper = styled.div;
+export const StyledButton = styled(IconButton)`
+    position: fixed;
+    float: right;
+    margin-bottom: 20px;
+    z-index: 100;
+`;
